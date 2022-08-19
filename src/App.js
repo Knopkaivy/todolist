25bf5d3887ba3aa283bb5ad7ebfb4cc1c027267d
Knopka <knopkaivy@gmail.com>
2022-08-19 07:56:23 -0500

    add background image to App component and not through css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import BgImage from './imgs/bg-lg.jpg';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
   return (
     <div className="App">
+      <div className="bg__container">
+        <img src={BgImage} alt="decorative background" className="bg__image" />
+      </div>
       <TodoList />
     </div>
   );
