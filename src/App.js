c58666d7bef8414c8455f957cd89cd3a7266c64f
Knopka <knopkaivy@gmail.com>
2022-08-19 17:16:13 -0500

    add routes and fixed broken code in auth components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import BgImage from './imgs/bg-lg.jpg';
+import Login from './Login';
+// import Register from './Register';
+import Reset from './Reset';
 import './styles/App.css';
 import TodoList from './TodoList';
 
@@ -9,7 +13,13 @@ function App() {
       <div className="bg__container">
         <img src={BgImage} alt="decorative background" className="bg__image" />
       </div>
-      <TodoList />
+
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="register" element={<Register />} /> */}
+        <Route path="reset" element={<Reset />} />
+      </Routes>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,53 +1,57 @@
-import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth, signInWithPopup,
- signInWithEmailAndPassword,
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
-  signOut
-​​} from "firebase/auth"; import {
+  signOut,
+} from 'firebase/auth';
+import {
   getFirestore,
   query,
   getDocs,
   collection,
   where,
- addDoc,​} from "firebase/firestore";
+  addDoc,
+} from 'firebase/firestore';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3GQ3ZrrbuiSAERZ8Jo2LJdDphnlHmzqQ",
-  authDomain: "todo-list-7fc18.firebaseapp.com",
-  projectId: "todo-list-7fc18",
-  storageBucket: "todo-list-7fc18.appspot.com",
-  messagingSenderId: "393279604140",
-  appId: "1:393279604140:web:698c64c7a0a9ee1982a609",
-  measurementId: "G-98T7KFQPXZ"
+  apiKey: 'AIzaSyA3GQ3ZrrbuiSAERZ8Jo2LJdDphnlHmzqQ',
+  authDomain: 'todo-list-7fc18.firebaseapp.com',
+  projectId: 'todo-list-7fc18',
+  storageBucket: 'todo-list-7fc18.appspot.com',
+  messagingSenderId: '393279604140',
+  appId: '1:393279604140:web:698c64c7a0a9ee1982a609',
+  measurementId: 'G-98T7KFQPXZ',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-​​const auth = getAuth(app);
-​​const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
     const res = await signInWithPopup(auth, googleProvider);
     const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
+      await addDoc(collection(db, 'users'), {
         uid: user.uid,
         name: user.displayName,
-        authProvider: "google",
+        authProvider: 'google',
         email: user.email,
       });
     }
@@ -68,10 +72,10 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users"), {
+    await addDoc(collection(db, 'users'), {
       uid: user.uid,
       name,
-      authProvider: "local",
+      authProvider: 'local',
       email,
     });
   } catch (err) {
@@ -82,7 +86,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
 const sendPasswordReset = async (email) => {
   try {
     await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
+    alert('Password reset link sent!');
   } catch (err) {
     console.error(err);
     alert(err.message);
@@ -97,6 +101,6 @@ export {
   signInWithGoogle,
   logInWithEmailAndPassword,
   registerWithEmailAndPassword,
-  sendPasswordReset,
+  sendPasswordResetEmail,
   logout,
 };
