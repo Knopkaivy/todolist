2e4f42ed0e08f8c1dd30746a60763c37b250abae
Knopka <knopkaivy@gmail.com>
2022-09-10 18:18:02 -0500

    add .env file and update firebaseConfig object for .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -23,14 +23,24 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyA3GQ3ZrrbuiSAERZ8Jo2LJdDphnlHmzqQ',
+//   authDomain: 'todo-list-7fc18.firebaseapp.com',
+//   projectId: 'todo-list-7fc18',
+//   storageBucket: 'todo-list-7fc18.appspot.com',
+//   messagingSenderId: '393279604140',
+//   appId: '1:393279604140:web:698c64c7a0a9ee1982a609',
+//   measurementId: 'G-98T7KFQPXZ',
+// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyA3GQ3ZrrbuiSAERZ8Jo2LJdDphnlHmzqQ',
-  authDomain: 'todo-list-7fc18.firebaseapp.com',
-  projectId: 'todo-list-7fc18',
-  storageBucket: 'todo-list-7fc18.appspot.com',
-  messagingSenderId: '393279604140',
-  appId: '1:393279604140:web:698c64c7a0a9ee1982a609',
-  measurementId: 'G-98T7KFQPXZ',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
