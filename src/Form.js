3f6f2161184bff2c5a9d1c7daf869d89917d6ac9
Knopka <knopkaivy@gmail.com>
2022-09-17 16:49:44 -0500

    Form component clean out commented out code

--- src/Form.js
+++ src/Form.js
@@ -3,55 +3,6 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import firestore from './FirebaseConfig';
 import './styles/Form.css';
 
-// class Form extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: '',
-//     };
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   componentDidMount() {
-//     if (this.props.itemValue) {
-//       this.setState({ value: this.props.itemValue });
-//     }
-//   }
-//   handleChange(event) {
-//     this.setState({ value: event.target.value });
-//   }
-//   handleSubmit(event) {
-//     event.preventDefault();
-//     if (this.props.id) {
-//       if (this.state.value === '') {
-//         this.props.deleteItem(this.props.id);
-//       } else {
-//         this.props.editItem(this.state.value, this.props.id);
-//       }
-//     } else {
-//       if (this.state.value !== '') {
-//         this.props.addItem(this.state.value);
-//         this.setState({ value: '' });
-//       } else return;
-//     }
-//   }
-//   render() {
-//     return (
-//       <form className="Form" onSubmit={this.handleSubmit}>
-//         <input
-//           id="formInput"
-//           onChange={this.handleChange}
-//           type="text"
-//           placeholder={this.props.placeholder ? this.props.placeholder : null}
-//           value={this.state.value}
-//           className="inpt"
-//         />
-//         <button className="btn btn-green">{this.props.buttonValue}</button>
-//       </form>
-//     );
-//   }
-// }
 const Form = ({
   addItem,
   buttonValue,
