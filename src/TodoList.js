08a91d3913dcd9873588eb37c7991cacc3b034e0
Knopka <knopkaivy@gmail.com>
2022-09-20 08:04:44 -0500

    clean up commented out code in TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 import Todo from './Todo';
 import Form from './Form';
 import './styles/TodoList.css';
@@ -12,64 +11,8 @@ const TodoList = ({
   editItem,
   todos,
 }) => {
-  // const [list, setList] = useState(todos);
-
-  // const addItem = (newVal) => {
-  //   let additionalItem = {
-  //     id: uuidv4(),
-  //     text: newVal,
-  //     completed: false,
-  //     editMode: false,
-  //   };
-  //   setTodos([...list, additionalItem]);
-  //   handleAddTodo(additionalItem);
-  // };
-
-  // const completeItem = (id) => {
-  //   const updatedList = todos.filter((item) => {
-  //     if (item.id !== id) return item;
-  //     else {
-  //       let newItem = item;
-  //       newItem.completed = newItem.completed ? false : true;
-  //       return newItem;
-  //     }
-  //   });
-  //   setTodos([...updatedList]);
-  // };
-
-  // const deleteItem = (id) => {
-  //   const updatedList = todos.filter((item) => {
-  //     return item.id !== id;
-  //   });
-  //   setTodos([...updatedList]);
-  // };
-
-  // const editModeOn = (id) => {
-  //   const updatedList = todos.filter((item) => {
-  //     if (item.id !== id) return item;
-  //     else {
-  //       let newItem = item;
-  //       newItem.editMode = true;
-  //       return newItem;
-  //     }
-  //   });
-  //   setTodos([...updatedList]);
-  // };
-
-  // const editItem = (newVal, id) => {
-  //   const newList = todos.filter((item) => {
-  //     if (item.id !== id) return item;
-  //     else {
-  //       let newItem = item;
-  //       newItem.text = newVal;
-  //       newItem.editMode = false;
-  //       newItem.completed = false;
-  //       return newItem;
-  //     }
-  //   });
-  //   setTodos([...newList]);
-  // };
   let itemList;
+
   if (todos && todos.length > 0) {
     itemList = todos.map((item) => {
       return (
